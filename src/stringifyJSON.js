4634a94153a511f370394ce86daa346b52c3cf07
Lightining1boy <spencer82188@gmail.com>
2019-04-08 21:48:01 -0700

    Almost done with stringify

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,60 @@
 
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function(obj) {
-  // your code goes here
+var stringifyJSON = function (obj) {
+
+  if (typeof obj === "string") {
+    return `"${obj}"`;
+    // var splitString = obj.split('');
+    // splitString.unshift('"')
+    // splitString.push('"')
+    // return splitString.join('');
+  }
+  if (obj === null) {
+    return 'null';
+  }
+  if (typeof obj === 'number' || typeof obj === "boolean") {
+    return '' + obj;
+  }
+  if (obj.length === 0) {
+    return '[]';
+  }
+  if (typeof obj === 'function' || obj === 'undefined') {
+    return '{}';
+  }
+
+  var newArr = [];
+  if (Array.isArray(obj)) {
+    for (var i = 0; i < obj.length; i++) {
+      if (typeof obj[i] === 'number' || typeof obj[i] === 'boolean') {
+        newArr.push(obj[i]);
+      } else {
+        newArr.push(stringifyJSON(obj[i]));
+      }
+    }
+    return `[${newArr}]`;
+
+  }
+  if (typeof obj === 'object') {
+    if (Object.keys(obj).length === 0) {
+      return '{}';
+    }
+    for (var key in obj) {  //{a: {b:c}}
+
+      var newKey = `"${key}"`;
+      var value = obj[key];
+      var objResult = '';
+      if (typeof value === 'number' || typeof value === 'boolean') {
+        objResult += newKey + ':' + value + ',';
+      } else {
+        objResult += newKey + ':' + stringifyJSON(value) + ',';
+      }
+    }
+    objResult = objResult.split('').splice(0, objResult.length - 1).join('');
+
+    return `{${objResult}}`;
+  }
+
+
+
 };
